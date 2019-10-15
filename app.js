9029a548aa1356c04e94413016d96d1265d934d2
Karen Daniela López <Karen_lopez1936@hotmail.com>
2019-10-15 00:10:51 -0700

    desarrollo del sitio web

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const Photo = document.getElementById('photo');
 const FirstName = document.getElementById('firstname');
 const SecondName = document.getElementById('secondname');
 const LastName = document.getElementById('lastname');
 const Age = document.getElementById('age');
 const Description = document.getElementById('description');
-const Photo = document.getElementById('photo');
 
 const ApiUrl = 'https://localhost:5001/'
 
@@ -20,14 +20,176 @@ const ApiUrl = 'https://localhost:5001/'
         const {firstName, secondName, lastName, age, description} = simpsons;
         const photo = simpsons;
         
+        Photo.src = photo;
         FirstName.innerText = firstName;
         SecondName.innerText = secondName;
         LastName.innerText = lastName;
         Age.innerText = age;
         Description.innerText = description;
-        Photo.src = photo;
     
     };
     
     getsimpsonsData();
+
+    //
+
+const Photo = document.getElementById('photo');
+const FirstName = document.getElementById('firstname');
+const SecondName = document.getElementById('secondname');
+const LastName = document.getElementById('lastname');
+const Age = document.getElementById('age');
+const Description = document.getElementById('description');
+
+    let getsimpsonsData = async ()=> {
+        const response = await fetch(`${ApiUrl}simpsons/character/2`);
+        const simpsons = await response.json();
+        console.log(simpsons);
+        const {firstName, secondName, lastName, age, description} = simpsons;
+        const photo = simpsons;
+        
+        Photo.src = photo;
+        FirstName.innerText = firstName;
+        SecondName.innerText = secondName;
+        LastName.innerText = lastName;
+        Age.innerText = age;
+        Description.innerText = description;
+    
+    };
+    
+    getsimpsonsData();
+
+    //
+
+const Photo = document.getElementById('photo');
+const FirstName = document.getElementById('firstname');
+const SecondName = document.getElementById('secondname');
+const LastName = document.getElementById('lastname');
+const Age = document.getElementById('age');
+const Description = document.getElementById('description');
+
+    let getsimpsonsData = async ()=> {
+        const response = await fetch(`${ApiUrl}simpsons/character/2`);
+        const simpsons = await response.json();
+        console.log(simpsons);
+        const {firstName, secondName, lastName, age, description} = simpsons;
+        const photo = simpsons;
+        
+        Photo.src = photo;
+        FirstName.innerText = firstName;
+        SecondName.innerText = secondName;
+        LastName.innerText = lastName;
+        Age.innerText = age;
+        Description.innerText = description;
+    
+    };
+    
+    getsimpsonsData();
+
+    //
+
+const Photo = document.getElementById('photo');
+const FirstName = document.getElementById('firstname');
+const SecondName = document.getElementById('secondname');
+const LastName = document.getElementById('lastname');
+const Age = document.getElementById('age');
+const Description = document.getElementById('description');
+
+    let getsimpsonsData = async ()=> {
+        const response = await fetch(`${ApiUrl}simpsons/character/2`);
+        const simpsons = await response.json();
+        console.log(simpsons);
+        const {firstName, secondName, lastName, age, description} = simpsons;
+        const photo = simpsons;
+        
+        Photo.src = photo;
+        FirstName.innerText = firstName;
+        SecondName.innerText = secondName;
+        LastName.innerText = lastName;
+        Age.innerText = age;
+        Description.innerText = description;
+    
+    };
+    
+    getsimpsonsData();
+
+    //
+const Photo = document.getElementById('photo');
+const FirstName = document.getElementById('firstname');
+const SecondName = document.getElementById('secondname');
+const LastName = document.getElementById('lastname');
+const Age = document.getElementById('age');
+const Description = document.getElementById('description');
+
+    let getsimpsonsData = async ()=> {
+        const response = await fetch(`${ApiUrl}simpsons/character/2`);
+        const simpsons = await response.json();
+        console.log(simpsons);
+        const {firstName, secondName, lastName, age, description} = simpsons;
+        const photo = simpsons;
+        
+        Photo.src = photo;
+        FirstName.innerText = firstName;
+        SecondName.innerText = secondName;
+        LastName.innerText = lastName;
+        Age.innerText = age;
+        Description.innerText = description;
+    
+    };
+    
+    getsimpsonsData();
+
+    //
+
+const Photo = document.getElementById('photo');
+const FirstName = document.getElementById('firstname');
+const SecondName = document.getElementById('secondname');
+const LastName = document.getElementById('lastname');
+const Age = document.getElementById('age');
+const Description = document.getElementById('description');
+
+    let getsimpsonsData = async ()=> {
+        const response = await fetch(`${ApiUrl}simpsons/character/2`);
+        const simpsons = await response.json();
+        console.log(simpsons);
+        const {firstName, secondName, lastName, age, description} = simpsons;
+        const photo = simpsons;
+        
+        Photo.src = photo;
+        FirstName.innerText = firstName;
+        SecondName.innerText = secondName;
+        LastName.innerText = lastName;
+        Age.innerText = age;
+        Description.innerText = description;
+    
+    };
+    
+    getsimpsonsData();
+
+    //
+
+const Photo = document.getElementById('photo');
+const FirstName = document.getElementById('firstname');
+const SecondName = document.getElementById('secondname');
+const LastName = document.getElementById('lastname');
+const Age = document.getElementById('age');
+const Description = document.getElementById('description');
+
+    let getsimpsonsData = async ()=> {
+        const response = await fetch(`${ApiUrl}simpsons/character/2`);
+        const simpsons = await response.json();
+        console.log(simpsons);
+        const {firstName, secondName, lastName, age, description} = simpsons;
+        const photo = simpsons;
+        
+        Photo.src = photo;
+        FirstName.innerText = firstName;
+        SecondName.innerText = secondName;
+        LastName.innerText = lastName;
+        Age.innerText = age;
+        Description.innerText = description;
+    
+    };
+    
+    getsimpsonsData();
+
 
